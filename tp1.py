42d929d7035a18495e524a8348c986459bc7dc38
Franco Suarez <francosz.2006@gmail.com>
2026-09-22 14:26:34 -0300

    Punto 1, estructura con datos de cada columna

---
+++ tp1.py
@@ -0,0 +1,17 @@
+
+#Punto 1:Generá una estructura que almacene los datos de cada columna: nombre, el tipo 
+#de dato y el porcentaje de completitud de cada una.
+
+columnas = {
+    'PONDERA': ['int', 95.0],
+    'ESTADO': ['int', 80.0],
+    'CAT_OCUP': ['int', 70.0],
+    'EDAD': ['int', 100.0],
+    'REGION': ['int', 98.5],
+    'AGLOMERADO': ['int', 98.5],
+    'MAS_500': ['string', 60.0],
+    'ANO4': ['int', 100.0],
+    'TRIMESTRE': ['int', 100.0],
+    'ITF': ['int', 45.0],
+    'GDECCFR': ['int', 50.0]
+}
